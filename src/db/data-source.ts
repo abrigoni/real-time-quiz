85f6d37a21188638bfd669b0d479118212e4f72f
abrigoni <brian.rigoni1@gmail.com>
2024-02-02 00:09:09 +0100

    refactor(user): module & table name

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,3 +1,4 @@
+import { User } from 'src/users/user.entity';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
@@ -7,7 +8,7 @@ export const dataSourceOptions: DataSourceOptions = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [],
+  entities: [User],
   synchronize: false,
 };
 
