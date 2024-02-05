58ca3984689a0494e5a8fac34c61c1f61bcde442
abrigoni <brian.rigoni1@gmail.com>
2024-02-05 02:46:25 +0100

    feat(app): nats connection

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Nats connection
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: [process.env.NATS_URL],
+    },
+  });
+  // Swagger doc
   const config = new DocumentBuilder()
     .setTitle('Real Time Quiz')
     .setDescription('API Description')
